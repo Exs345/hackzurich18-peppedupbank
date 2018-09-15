44f3c92304449fef5f2bd4da2ec9a7fd30c38e2c
Exs345 <antonhasenkampf@gmx.de>
2018-09-15 14:54:23 +0200

    Add files via upload
    Writes ID as a number

---
+++ pepperrun.py
@@ -0,0 +1,25 @@
+
+import http.client
+def pep(number):
+    conn = http.client.HTTPSConnection("csopenbankingzh.azurewebsites.net")
+
+    payload = "{ \n\t\n\t\"filter\" : [\t\n\t{\n\t\"attribute\" : \"kyc.job\" ,\n\t\"operator\" : \"like\" ,\n\t\"value\" : \"'%RETIRED%'\" \n\t},\n\t{\n\t\"attribute\" : \"id\" ,\n\t\"operator\" : \"like\" ,\n\t\"value\" : 600014 \n\t} \n\t] ,\n\t\n\t\"sorting\" : \" nickname asc\"\n\n}"
+    str(number)
+    #print(payload)
+    payload = payload[:161] + str(number) + payload[169:]
+    #payload[162:168]=number
+    #print(payload)
+    headers = {
+        'content-type': "application/json",
+        'accept': "application/json",
+        'authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9UUXlRVGRCTkRFNU1rWTNNMFZFTnpNeU9EWkJOa1pETkRCR1FrRkZOamxDTTBJeE5EazRNdyJ9.eyJpc3MiOiJodHRwczovL2dicHJvamVjdC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWI5Y2QwZTc3MmQ0YmI0N2Y5YTdjZmZlIiwiYXVkIjpbImh0dHBzOi8vb3BlbmJhbmtpbmcuY29tL2FwaSIsImh0dHBzOi8vZ2Jwcm9qZWN0LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1MzcwMDM4OTgsImV4cCI6MTUzNzA5MDI5OCwiYXpwIjoicThJTVlzMVNLekt0WExvdjhWM0pab3oxaE43MHgzQTciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiZ3R5IjoicGFzc3dvcmQifQ.Ko7O6k5zVBgE7zjh4qREM82y8v-_Crz2QzR-m_-xgUk7e0-7vKceAuBZfEuMxW2Le5wx5oP3xI6h-SIEQ7Lnfzsy1N8gZijsSymjKl_u6N-qiUu3iC0NiRmnDauQ3a8blz6aPrJtWs_j7XnLnp5KPALSzhH-zpxg8XwmJaHIF18JNp3J1w0lupTN28nue1zAfIxWFLweS_Vl894CV5K4hQU08wx2YT7_4XO7TMqAxpx4o-eTn2qBtiDGq0lx7sEwwHgqqT2z6gTBiHy9Y4Zl7prUihPyIh3GvhERg-J7_T5HeFPmAxcqH_RboxBE_34H__bLjj8rKCsMZ31-iZ8Haw",
+        'cache-control': "no-cache",
+        'postman-token': "8750c4af-6000-f48b-0a73-9954e95cfcb5"
+        }
+
+    conn.request("POST", "/customers/search/unsecured", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
