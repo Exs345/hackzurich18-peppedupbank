a7bdb6b73f3b0c58067294c6c41d5525887b84ca
Beatriz Borges <beatrizmbribeiro@gmail.com>
2018-09-15 15:50:23 +0200

    Various slight tweaks

---
+++ lib/config.py
@@ -0,0 +1,3 @@
+COGNITIVE_FACE_KEY = '024b1b187692427c8dc756fb1e0be75e'
+COGNITIVE_FACE_BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'
+COGNITIVE_FACE_LARGE_PERSON_GROUP_ID = 'hackzurich2018'
